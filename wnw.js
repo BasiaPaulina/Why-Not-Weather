891edc6dd951fa92fb497dfa42d4bc9f6c3b1da9
BasiaPaulina <48137639+BasiaPaulina@users.noreply.github.com>
2019-04-12 00:13:59 +0100

    formatted date and time

--- wnw.js
+++ wnw.js
@@ -10,10 +10,32 @@ console.log(`${url}/${path}?${appParams}`);
 axios.get(`${url}/${path}?${appParams}`).then(function(response) {
   let currentTemperature = Math.round(response.data.main.temp);
   let currentWindSpeed = `${response.data.wind.speed} m/s`;
-  let description = response.data.weather[2];
+  let description = response.data.weather[0].description;
   let currentPericipation = response.data.rain;
   let location = document.querySelector(".location-h1");
   location.innerHTML = city;
   let mainWeather = document.querySelector(".main-weather");
   mainWeather.innerHTML = `${currentTemperature} C ${currentWindSpeed} ${description} ${currentPericipation}`;
 });
+
+let now = new Date();
+function formatDate(date) {
+  let weekDays = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let year = date.getFullYear();
+  let weekDay = weekDays[date.getDay()];
+  let dayOfMonth = date.getDate();
+  return `${dayOfMonth} ${weekDay} ${year}`;
+}
+
+function formatTime(time) {
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  let fullTime = `${hour}:${minute}`;
+  if (hour >= 10) {
+    return fullTime;
+  } else {
+    return `0${fullTime}`;
+  }
+}
+let date = document.querySelector(".date");
+date.innerHTML = `${formatDate(now)} ${formatTime(now)}`;
