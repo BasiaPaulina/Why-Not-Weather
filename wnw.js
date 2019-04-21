4a6b82cd3b04c882062533e25245c5ec92a00ea0
BasiaPaulina <48137639+BasiaPaulina@users.noreply.github.com>
2019-04-21 18:18:56 +0100

    fixed search

--- wnw.js
+++ wnw.js
@@ -1,17 +1,13 @@
+//elements
 let mainTemperature = document.querySelector("#temperature-main");
 let mainWindSpeed = document.querySelector("#wind-speed-main");
 let mainDescription = document.querySelector("#description-main");
 let mainHumidity = document.querySelector("#humidity-main");
 let timeStamp = document.querySelector("#weather-date");
 let mainCity = document.querySelector("#location-h1");
-let apiKey = "ace2c200d6c61096c76082a9e2846e29";
-let url = "https://api.openweathermap.org/data/2.5/";
-let path = "weather";
-let city = "Lisbon";
-let units = "metric";
-let appParams = `q=${city}&appid=${apiKey}&units=${units}`;
-console.log(`${url}/${path}?${appParams}`);
+let search = document.querySelector("#city-form");
 
+//date function
 function formatDate(date) {
   let weekDays = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   let year = date.getFullYear();
@@ -33,11 +29,23 @@ function formatDate(date) {
   }
 }
 
-axios.get(`${url}/${path}?${appParams}`).then(function(response) {
-  mainTemperature.innerHTML = Math.round(response.data.main.temp);
-  mainWindSpeed.innerHTML = `${response.data.wind.speed} km/h`;
-  mainDescription.innerHTML = response.data.weather[0].description;
-  mainHumidity.innerHTML = `${response.data.main.humidity}%`;
-  timeStamp.innerHTML = formatDate(new Date(response.data.dt * 1000));
-  mainCity.innerHTML = city;
-});
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let apiKey = "ace2c200d6c61096c76082a9e2846e29";
+  let url = "https://api.openweathermap.org/data/2.5/";
+  let path = "weather";
+  let units = "metric";
+  let appParams = `q=${city}&appid=${apiKey}&units=${units}`;
+  console.log(`${url}/${path}?${appParams}`);
+  axios.get(`${url}/${path}?${appParams}`).then(function(response) {
+    mainTemperature.innerHTML = Math.round(response.data.main.temp);
+    mainWindSpeed.innerHTML = `${response.data.wind.speed} km/h`;
+    mainDescription.innerHTML = response.data.weather[0].description;
+    mainHumidity.innerHTML = `${response.data.main.humidity}%`;
+    timeStamp.innerHTML = formatDate(new Date(response.data.dt * 1000));
+    mainCity.innerHTML = city;
+  });
+}
+
+search.addEventListener("submit", handleSearch);
